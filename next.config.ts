1b9b43769184fa67c9080cd72cc5a225a9b32d2b
LuiS3112v <natanielganda141@gmail.com>
2026-05-16 23:13:26 +0100

    add vercel build config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
+  experimental: {
+    optimizePackageImports: [
+      "lucide-react",
+      "@radix-ui/react-icons"
+    ]
+  },
+  eslint: {
+    ignoreDuringBuilds: true
+  }
 };
 
 export default nextConfig;
